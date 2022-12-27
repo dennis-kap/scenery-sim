c7aee9c8fabd3f12ff940a515a98a804b0c0c2a2
dennisicecap1 <102987428+dennis-kap@users.noreply.github.com>
2022-12-27 00:40:24 -0500

    Organized code and added more comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/* Importing React, CSS sheet and makeRain functions */
 import React from 'react';
 import './App.css';
 import { Rain, ChangeType, ToggleRain } from './makeRain';
 
+/* Importing images to allow changing urls */
 import summer from './Images/summer.png';
 import spring from './Images/spring.png';
 import fall from './Images/fall.png';
@@ -15,31 +17,35 @@ import treeWi from './Images/treeWi.png';
 import day from './Images/day.png';
 import night from './Images/night.png';
 
+/* Setting default images */
 var hills = summer;
 var tree = treeSu;
 var time = day;
 
+/* App */
 class App extends React.Component {
 
+  /* Function gets the angle from the range slider input */
+  /* With the angle, sets the swaying angle of plants and the rain angle using document style setProperty */
   changeSkew(angle){
     let angleBack = -angle;
     angleBack = 0.2 * angleBack;
     let degrees = 'deg';
 
-    let precipAngle = angle.toString().concat(degrees);
+    /* Precipitation angle is set before the angle for the plants are set to a decreased angle */
+    let precipAngle = angle + degrees;
 
     angle = 0.2 * angle;
-
-    let angleStr = angle.toString().concat(degrees);
-    let angleBackStr = angleBack.toString().concat(degrees);
+    let angleStr = angle + degrees;
+    let angleBackStr = angleBack + degrees
     
     document.documentElement.style.setProperty('--skew-amount', angleStr);
     document.documentElement.style.setProperty('--skew-back-amount', angleBackStr);
-
     document.documentElement.style.setProperty('--rotate-rain-amount', precipAngle);
 
   }
 
+  /* Function that recieves the season radio input from the user and changes the images accordingly */
   changeSeason(season) {
     if(season === "fall"){
       hills = fall;
@@ -58,17 +64,22 @@ class App extends React.Component {
       tree = treeSu;
     }
     
+    /* Fall speed is also changes to make snow fall slower than rain */
     if(season === "winter"){
       document.documentElement.style.setProperty('--fall-speed', "1s");
     }
     else{
       document.documentElement.style.setProperty('--fall-speed', "0.5s");
     }
+
+    /* Precipitation type is changed */
     ChangeType(season);
 
+    /* Force update allows the images to be changed as they are rendered again */
     this.forceUpdate();
   }
 
+  /* Function that will change the brightness and background based on radio input from the user */
   changeTime(t) {
     if(t === "day"){
       time = day;
@@ -81,35 +92,54 @@ class App extends React.Component {
     this.forceUpdate();
   }
 
+  /* The HTML layout of the program */
   render() {
     return (
+
+      /* Container for the entire site is screen and container for the scene is scene */
       <div id="screen">
         <div id="scene">
           <div>
+
+            {/* Sky and hill background/foreground are created */}
             <img id="sky" src={time} alt=""></img>
             <img id="hill" src={hills} alt="summer"></img>
           </div>
 
           <div>
+            {/* Tree is added */}
             <img id="tree" class="plant" src={tree} alt=""></img>
           </div>
+
+          {/* Container and precipitation elements are created */}
           <div class="precipitation">
             <Rain />
           </div>
         </div>
         <div>
+
+          {/* Container for user inputs */}
           <ul id="inputs">
             <li>
+
+              {/* Inputs for wind (range slider) */}
+              {/* On input, changeSkew function is called */}
               <h3>Wind</h3>
               <input type="range" id="wind" min="1" max="15" defaultValue="1"
                 onInput={()=>this.changeSkew(document.getElementById('wind').value)}></input>
             </li>
             <li>
+
+              {/* Input for precipitation (checkbox) */}
+              {/* On user input, ToggleRain function is called */}
               <h3>Precipitation</h3>
               <input type="checkbox" id="togglePrecip"
                 onInput={()=>ToggleRain(document.getElementById('togglePrecip').checked)}></input>
             </li>
             <li>
+
+              {/* Inputs for season */}
+              {/* On input, changeSeason function is called */}
               <h3>Season</h3>
               <form>
                 <input type="radio" id="spring" name="season"
@@ -127,6 +157,9 @@ class App extends React.Component {
               </form>
             </li>
             <li>
+
+              {/* Inputs for time of day (radio buttons with day/night option) */}
+              {/* On input, changeTime function is called */}
               <h3>Time of Day</h3>
               <form>
                 <input type="radio" id="day" name="time" defaultChecked
@@ -145,4 +178,5 @@ class App extends React.Component {
   }
 }
 
+/* Return result is exported */ 
 export default App;
--- src/makeRain.js
+++ src/makeRain.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+/* Snow and rain pngs are imported */ 
 import snow1 from './Images/snow1.png';
 import snow2 from './Images/snow2.png';
 import snow3 from './Images/snow3.png';
@@ -8,36 +9,48 @@ import rain1 from './Images/rain1.png';
 import rain2 from './Images/rain2.png';
 import rain3 from './Images/rain3.png';
 
+/* Arrays of snow and rain images are made to iterate through the images */
 const snow = [snow1, snow2, snow3];
-const rain = [rain1, rain2, rain3, rain3];
+const rain = [rain1, rain2, rain3];
 
+/* Default precipitation type is created */
 var type = rain;
 
-var particles = [];
-
+  /* Rain function that iterates up to 300 times to create precipitation elements */
   function Rain() {
-    particles = [];
+
+    /* Returned array for precipitation creation is initialized */
+    var particles = [];
+
+    /* Iterator is initialized */
     var i = 0;
     while(i < 300) {
+
+      /* Iterator randomly incremented */
       var randomInc = (Math.floor(Math.random() * 3) + 1);
       i += randomInc;
 
+      /* Random X and Y values are created. randomT delays the animation of each element randomly */
       var randomX = (Math.random() * 100) + "%";
       var randomY = (Math.random() * 100) * 1000 + "px";
       var randomT = (Math.random() * 1000) + "ms";
+
+      /* randomIndex chooses a random element from the arrays of size 3 */
       const randomIndex = Math.floor(Math.random() * 3); 
+      /* randomSize chooses a random size for the element */
       const randomSize = (randomIndex * 20) + "px";
 
+      /* The elements are added to the particles array */
       particles.push(<img id="precip" 
       style={{left: randomX, bottom: randomY, animationDelay: randomT}}
       height={randomSize} src={type[randomIndex]} alt=""/>);
     }
 
-    console.log(document.documentElement.style.getPropertyValue('--rain-opacity'));
-
+    /* Array returned to add as HTML elements */
     return particles;
   }
 
+  /* Function that changes seasons by changing the precipitation type based on user input */
   function ChangeType(season){
     if(season === "winter"){
       type = snow;
@@ -47,6 +60,8 @@ var particles = [];
     }
   }
 
+  /* Function that toggles whether precipitation is visible or not. */
+  /* Done by changing the display value between inline or none if the presses the checkbox on the page */
   function ToggleRain(toggle){
     if(document.documentElement.style.getPropertyValue('--rain-opacity') === "inline") {
       document.documentElement.style.setProperty('--rain-opacity', "none");
@@ -56,4 +71,5 @@ var particles = [];
     }
   }
 
+/* Functions exported for use in main js file */
 export {Rain, ChangeType, ToggleRain};
